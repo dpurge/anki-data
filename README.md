c3ced405368a85b1594df358c640758d5d04927e
Janusz Prusaczyk (JJPR) <jjpr@demant.com>
2020-05-20 02:08:19 +0200

    Add Arabic from Italki lesson 1

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# anki-data
-Data for Anki flashcards
+# Anki Data
 
+Generic project for my Anki flashcards.
+
+Store data in various formats, apply templates, render markdown, use images, sounds and video clips.
+
+All files use `utf-8` encoding.
+
+## Setup
 
 ```powershell
 python -m venv .venv
@@ -11,4 +17,20 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 
 deactivate
+```
+
+## Generating identifiers
+
+To generate Anki ID:
+
+```python
+import random
+random.randrange(1 << 30, 1 << 31)
+```
+
+To generate GUID:
+
+```python
+import uuid
+str(uuid.uuid4())
 ```
--- tasks.py
+++ tasks.py
@@ -13,6 +13,54 @@ out_dir = os.path.abspath('./out')
 anki_packages = [
 
     AnkiPackage(
+        filename = os.path.join(out_dir, 'language', 'modern-standard-arabic.apkg'),
+        deck = AnkiDeck(
+                id = 1706626659,
+                name = 'Language::Modern Standard Arabic',
+                notes = [
+                    AnkiNote(
+                        model = os.path.join(src_dir, 'anki', 'lang', 'arb', 'basic', 'model.json'),
+                        template = os.path.join(src_dir, 'template', 'lang', 'arb', 'vocabulary-basic', 'config.json'),
+                        data = [
+                            os.path.join(src_dir, 'data', 'lang', 'arb', 'vocabulary')
+                        ]
+                    ),
+                    AnkiNote(
+                        model = os.path.join(src_dir, 'anki', 'lang', 'arb', 'cloze', 'model.json'),
+                        template = os.path.join(src_dir, 'template', 'lang', 'arb', 'models-cloze', 'config.json'),
+                        data = [
+                            os.path.join(src_dir, 'data', 'lang', 'arb', 'models')
+                        ]
+                    )
+                ]
+        )
+    ),
+
+    AnkiPackage(
+        filename = os.path.join(out_dir, 'language', 'egyptian-arabic.apkg'),
+        deck = AnkiDeck(
+                id = 1851605761,
+                name = 'Language::Egyptian Arabic',
+                notes = [
+                    AnkiNote(
+                        model = os.path.join(src_dir, 'anki', 'lang', 'arz', 'basic', 'model.json'),
+                        template = os.path.join(src_dir, 'template', 'lang', 'arz', 'vocabulary-basic', 'config.json'),
+                        data = [
+                            os.path.join(src_dir, 'data', 'lang', 'arz', 'vocabulary')
+                        ]
+                    ),
+                    AnkiNote(
+                        model = os.path.join(src_dir, 'anki', 'lang', 'arz', 'cloze', 'model.json'),
+                        template = os.path.join(src_dir, 'template', 'lang', 'arz', 'models-cloze', 'config.json'),
+                        data = [
+                            os.path.join(src_dir, 'data', 'lang', 'arz', 'models')
+                        ]
+                    )
+                ]
+        )
+    ),
+
+    AnkiPackage(
         filename = os.path.join(out_dir, 'language', 'indonesian.apkg'),
         deck = AnkiDeck(
                 id = 1780195254,
