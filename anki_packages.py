282f6fe7a3ae4ee0bd37679bdec0851d635dfdbc
Janusz Prusaczyk (JJPR) <jjpr@demant.com>
2020-06-16 03:24:23 +0200

    Add Mandarin Chinese vocabulary and texts

--- anki_packages.py
+++ anki_packages.py
@@ -5,49 +5,81 @@ from ankiproject import AnkiPackage, AnkiDeck, AnkiNote
 def get_anki_packages(src_dir, out_dir):
 
     # ARB
-    yield AnkiPackage (
-        filename = os.path.join(out_dir, 'language', 'modern-standard-arabic.apkg'),
-        deck = AnkiDeck(
-                id = 1706626659,
-                name = 'Language::Modern Standard Arabic',
-                notes = [
-                    AnkiNote(
-                        model = os.path.join(src_dir, 'anki', 'lang', 'arb', 'basic', 'model.json'),
-                        template = os.path.join(src_dir, 'template', 'lang', 'arb', 'vocabulary-basic', 'config.json'),
-                        data = [
-                            os.path.join(src_dir, 'data', 'lang', 'arb', 'vocabulary')
-                        ]
-                    ),
-                    AnkiNote(
-                        model = os.path.join(src_dir, 'anki', 'lang', 'arb', 'cloze', 'model.json'),
-                        template = os.path.join(src_dir, 'template', 'lang', 'arb', 'models-cloze', 'config.json'),
-                        data = [
-                            os.path.join(src_dir, 'data', 'lang', 'arb', 'models')
-                        ]
-                    )
-                ]
-            )
-        )
+    # yield AnkiPackage (
+    #     filename = os.path.join(out_dir, 'language', 'modern-standard-arabic.apkg'),
+    #     deck = AnkiDeck(
+    #             id = 1706626659,
+    #             name = 'Language::Modern Standard Arabic',
+    #             notes = [
+    #                 AnkiNote(
+    #                     model = os.path.join(src_dir, 'anki', 'lang', 'arb', 'basic', 'model.json'),
+    #                     template = os.path.join(src_dir, 'template', 'lang', 'arb', 'vocabulary-basic', 'config.json'),
+    #                     data = [
+    #                         os.path.join(src_dir, 'data', 'lang', 'arb', 'vocabulary')
+    #                     ]
+    #                 ),
+    #                 AnkiNote(
+    #                     model = os.path.join(src_dir, 'anki', 'lang', 'arb', 'cloze', 'model.json'),
+    #                     template = os.path.join(src_dir, 'template', 'lang', 'arb', 'models-cloze', 'config.json'),
+    #                     data = [
+    #                         os.path.join(src_dir, 'data', 'lang', 'arb', 'models')
+    #                     ]
+    #                 )
+    #             ]
+    #         )
+    #     )
 
     # ARZ
+    # yield AnkiPackage (
+    #     filename = os.path.join(out_dir, 'language', 'egyptian-arabic.apkg'),
+    #     deck = AnkiDeck(
+    #             id = 1851605761,
+    #             name = 'Language::Egyptian Arabic',
+    #             notes = [
+    #                 AnkiNote(
+    #                     model = os.path.join(src_dir, 'anki', 'lang', 'arz', 'basic', 'model.json'),
+    #                     template = os.path.join(src_dir, 'template', 'lang', 'arz', 'vocabulary-basic', 'config.json'),
+    #                     data = [
+    #                         os.path.join(src_dir, 'data', 'lang', 'arz', 'vocabulary')
+    #                     ]
+    #                 ),
+    #                 AnkiNote(
+    #                     model = os.path.join(src_dir, 'anki', 'lang', 'arz', 'cloze', 'model.json'),
+    #                     template = os.path.join(src_dir, 'template', 'lang', 'arz', 'models-cloze', 'config.json'),
+    #                     data = [
+    #                         os.path.join(src_dir, 'data', 'lang', 'arz', 'models')
+    #                     ]
+    #                 )
+    #             ]
+    #         )
+    #     )
+
+    # CMN
     yield AnkiPackage (
-        filename = os.path.join(out_dir, 'language', 'egyptian-arabic.apkg'),
+        filename = os.path.join(out_dir, 'language', 'mandarin-chinese.apkg'),
         deck = AnkiDeck(
-                id = 1851605761,
-                name = 'Language::Egyptian Arabic',
+                id = 1973408674,
+                name = 'Language::Mandarin Chinese',
                 notes = [
                     AnkiNote(
-                        model = os.path.join(src_dir, 'anki', 'lang', 'arz', 'basic', 'model.json'),
-                        template = os.path.join(src_dir, 'template', 'lang', 'arz', 'vocabulary-basic', 'config.json'),
-                        data = [
-                            os.path.join(src_dir, 'data', 'lang', 'arz', 'vocabulary')
-                        ]
+                       model = os.path.join(src_dir, 'anki', 'lang', 'cmn', 'vocabulary', 'model.json'),
+                       template = os.path.join(src_dir, 'template', 'lang', 'cmn', 'vocabulary', 'config.json'),
+                       data = [
+                           os.path.join(src_dir, 'data', 'lang', 'cmn', 'short-term-spoken', 'vocabulary')
+                       ]
                     ),
                     AnkiNote(
-                        model = os.path.join(src_dir, 'anki', 'lang', 'arz', 'cloze', 'model.json'),
-                        template = os.path.join(src_dir, 'template', 'lang', 'arz', 'models-cloze', 'config.json'),
+                       model = os.path.join(src_dir, 'anki', 'lang', 'cmn', 'models', 'model.json'),
+                       template = os.path.join(src_dir, 'template', 'lang', 'cmn', 'models', 'config.json'),
+                       data = [
+                           os.path.join(src_dir, 'data', 'lang', 'cmn', 'short-term-spoken', 'models')
+                       ]
+                    ),
+                    AnkiNote(
+                        model = os.path.join(src_dir, 'anki', 'lang', 'cmn', 'texts', 'model.json'),
+                        template = os.path.join(src_dir, 'template', 'lang', 'cmn', 'texts', 'config.json'),
                         data = [
-                            os.path.join(src_dir, 'data', 'lang', 'arz', 'models')
+                            os.path.join(src_dir, 'data', 'lang', 'cmn', 'short-term-spoken', 'texts')
                         ]
                     )
                 ]
@@ -55,36 +87,36 @@ def get_anki_packages(src_dir, out_dir):
         )
 
     # IND
-    yield AnkiPackage (
-        filename = os.path.join(out_dir, 'language', 'indonesian.apkg'),
-        deck = AnkiDeck(
-                id = 1780195254,
-                name = 'Language::Indonesian',
-                notes = [
-                    AnkiNote(
-                        model = os.path.join(src_dir, 'anki', 'lang', 'ind', 'basic', 'model.json'),
-                        template = os.path.join(src_dir, 'template', 'lang', 'ind', 'vocabulary-basic', 'config.json'),
-                        data = [
-                            os.path.join(src_dir, 'data', 'lang', 'ind', 'vocabulary')
-                        ]
-                    ),
-                    AnkiNote(
-                        model = os.path.join(src_dir, 'anki', 'lang', 'ind', 'cloze', 'model.json'),
-                        template = os.path.join(src_dir, 'template', 'lang', 'ind', 'models-cloze', 'config.json'),
-                        data = [
-                            os.path.join(src_dir, 'data', 'lang', 'ind', 'models')
-                        ]
-                    )
-                ]
-            )
-        )
+    # yield AnkiPackage (
+    #     filename = os.path.join(out_dir, 'language', 'indonesian.apkg'),
+    #     deck = AnkiDeck(
+    #             id = 1780195254,
+    #             name = 'Language::Indonesian',
+    #             notes = [
+    #                 AnkiNote(
+    #                     model = os.path.join(src_dir, 'anki', 'lang', 'ind', 'basic', 'model.json'),
+    #                     template = os.path.join(src_dir, 'template', 'lang', 'ind', 'vocabulary-basic', 'config.json'),
+    #                     data = [
+    #                         os.path.join(src_dir, 'data', 'lang', 'ind', 'vocabulary')
+    #                     ]
+    #                 ),
+    #                 AnkiNote(
+    #                     model = os.path.join(src_dir, 'anki', 'lang', 'ind', 'cloze', 'model.json'),
+    #                     template = os.path.join(src_dir, 'template', 'lang', 'ind', 'models-cloze', 'config.json'),
+    #                     data = [
+    #                         os.path.join(src_dir, 'data', 'lang', 'ind', 'models')
+    #                     ]
+    #                 )
+    #             ]
+    #         )
+    #     )
 
     # LAT
-    yield AnkiPackage (
-        filename = os.path.join(out_dir, 'language', 'latin.apkg'),
-        deck = AnkiDeck (
-                id = 1228695891,
-                name = 'Language::Latin',
-                notes = []
-            )
-        )
+    # yield AnkiPackage (
+    #     filename = os.path.join(out_dir, 'language', 'latin.apkg'),
+    #     deck = AnkiDeck (
+    #             id = 1228695891,
+    #             name = 'Language::Latin',
+    #             notes = []
+    #         )
+    #     )
